045766915dda1f3a1964b0644cb9d74df48af66f
ihimawan <indri.himawan18@yahoo.com>
2019-09-30 20:11:45 -0400

    intro donctent

--- p1/script.js
+++ p1/script.js
@@ -1,10 +1,27 @@
+let gameStages = ['intro', 'chat', 'game', 'win', 'lose'];
+
 const app = new Vue({
     el: '#app',
     data: {
-        gameStage: 'intro', //chat //game // win // lose
+        currentGameStage: gameStages[0],
         playerInfo: {
-            name: '',
-            picture: ''
+            name: null,
+            validName: false,
+            selectedPictureIdx: null,
+            picturesAvailable: [
+                {
+                    img: "http://via.placeholder.com/200",
+                    comment: "What a handsome devil you are."
+                },
+                {
+                    img: "http://via.placeholder.com/200",
+                    comment: "You look bombin.. it must be the alpacalypse."
+                },
+                {
+                    img: "http://via.placeholder.com/200",
+                    comment: "mmm.... Feleece navidad.."
+                }
+            ]
         },
         chatInfo: {
 
@@ -17,6 +34,25 @@ const app = new Vue({
         },
         loseInfo: {
 
+        }
+    },
+    methods: {
+        selectProfilePicture: function (index) {
+            this.playerInfo.selectedPictureIdx = index;
+        },
+        validateName: function (name) {
+            if (name !== '' && name.length <= 10 && name.search(/[^a-zA-Z]+/) === -1) {
+                this.playerInfo.validName = true;
+            } else {
+                this.playerInfo.validName = false;
+            }
+        },
+        goToNextGameStage: function () {
+            let indexOfNextStage = gameStages.indexOf(this.currentGameStage);
+            if (indexOfNextStage !== -1 && gameStages.length <= indexOfNextStage + 2) {
+                throw new Error('Unable to find next stage');
+            }
+            this.currentGameStage = gameStages[indexOfNextStage + 1]
         }
     }
 });
