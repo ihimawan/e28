eb06e77b7c37ac018064ac934019c1df29a6ae90
ihimawan <indri.himawan18@yahoo.com>
2019-11-20 07:26:02 -0500

    update announcement

--- p3/src/components/HomePage/HelloPage/PreIntroPage/PreIntroPage.vue
+++ p3/src/components/HomePage/HelloPage/PreIntroPage/PreIntroPage.vue
@@ -11,6 +11,17 @@
           {{quote.source}}</cite></footer>
       </blockquote>
     </div>
+    <div>
+      <h1 class="display-4">
+        (11/20/19) New Chat Feature</h1>
+      For existing players, you might have been waiting for this update for ages. Don't worry, we are here for you. We are coding as fast as we can with our little Alpaca paws (hooves? idk).
+      Once you have finished the Llama and Alpaca test (if you've never been to this website, which how could you?, you'll find more about it once you've created a profile), you will be able
+      to chat with any of our users.
+      <img src="../../../../assets/images/preview/1.jpg" height="590" width="487" class="preview-image"><strong></strong>
+      Chat windows will be created for each user you interact with. So you can live your inner fantasy as a creepy stalker. Don't worry, we don't judge anyone here at Alpacan Mingle. Unless
+      you are a Llama. Llama bad.
+      <img src="../../../../assets/images/preview/2.jpg" width="600" class="preview-image"/>
+    </div>
     <a class="btn btn-primary btn-lg get-started-btn" href="#" role="button" @click="$emit('next')">
       Sounds intriguing... how can I join in the fun?</a>
     <div>
@@ -46,5 +57,14 @@ export default {
   }
   .get-started-btn {
     margin: 20px 20px 20px 0;
+  }
+  .preview-image{
+    display: block;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 10px;
+    border: #ffd8dd 2px solid;
   }
 </style>
