1389caa897e3cca31148140dd63394aa6d07ec0b
ihimawan <indri.himawan18@yahoo.com>
2019-12-07 08:35:17 -0500

    working?

--- p4/tests/unit/example.spec.js
+++ p4/tests/unit/example.spec.js
@@ -1,13 +1,35 @@
 import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import CoreGame from "../../src/components/GamePage/CoreGame/CoreGame";
+import Vuex from "vuex";
+import {createLocalVue, mount} from "@vue/test-utils";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('CoreGame', () => {
+  let profileCollection = [{
+    id: 1,
+    title: 'name1',
+    description: 'text1',
+    distance: 1,
+    speciesId: 1
+  },
+    {
+      id: 2,
+      title: 'name2',
+      description: 'text2',
+      distance: 2,
+      speciesId: 1
+    }]
+
+  it('shows one player card each time', () => {
+    const wrapper = mount(CoreGame, {
+      computed: {
+        profileCollection: function () {
+          return profileCollection
+        }
+      },
     })
-    expect(wrapper.text()).to.include(msg)
+    expect(wrapper.contains('ProfileCard')).to.be(true)
   })
 })
